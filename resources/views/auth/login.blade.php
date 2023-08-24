80ce73c9f1093905def26471f950a735a328284a
Muhammad Shariq Bin Zaid <shariqbinzaid@gmail.com>
2023-08-24 21:52:54 +0500

    2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,13 +64,11 @@
             <div class="d-flex flex-lg-row-fluid">
                 <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
                     <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
-                        src="{{ asset('assets/media/auth/fav.png') }}" alt="" />
-                    <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
                         src="{{ asset('assets/media/auth/logo.png') }}" alt="" />
                     <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20"
                         src="{{ asset('assets/media/auth/agency-dark.png') }}" alt="" />
 
-                    {{-- <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">Drone'</h1> --}}
+                    <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">Drone</h1>
 
                     <div class="text-gray-600 fs-base text-center fw-semibold">In this kind of post,
                         <a href="#" class="opacity-75-hover text-primary me-1">the blogger</a>introduces a person
