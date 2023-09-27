a4b2db0431200452da71178b579303076b3484d0
Muhammad Shariq Bin Zaid <shariqbinzaid@gmail.com>
2023-09-27 20:02:59 +0500

    1

--- app/Models/UserComments.php
+++ app/Models/UserComments.php
@@ -12,6 +12,6 @@ class UserComments extends Model
 
     public function getUser()
     {
-        return $this->hasMany(User::class, 'id', 'follower_id');
+        return $this->hasMany(User::class, 'user_id', 'id');
     }
 }
