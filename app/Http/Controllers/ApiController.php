30202a388dda0cd9cbefbf53631b7dfc59b84dfd
Muhammad Shariq Bin Zaid <shariqbinzaid@gmail.com>
2023-10-20 02:27:11 +0500

    2

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -148,19 +148,18 @@ class ApiController extends Controller
     // }
 
     public function login(Request $request)
-{
-    if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-        $user = Auth::user();
-        $success['token'] =  $user->createToken('MyApp')->accessToken;
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->accessToken;
 
-        // Fetch the currently logged-in user along with 'getCategory' relationship
-        $success['user'] =  User::with('getCategory')->find($user->id);
+            $success['user'] =  User::with('getCategory')->find($user->id);
 
-        return $this->sendResponse($success, 'User Login Successfully.');
-    } else {
-        return $this->sendResponse('Unauthorised.', ['error' => 'Email or Password Incorrect']);
+            return $this->sendResponse($success, 'User Login Successfully.');
+        } else {
+            return $this->sendResponse('Unauthorised.', ['error' => 'Email or Password Incorrect']);
+        }
     }
-}
 
 
     public function userupdate(Request $req)
